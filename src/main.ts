5491c90ab1b5cc100a806b2f89c75bffbb9ae11f
Fabian Montoya <37662924+FabianMontoya@users.noreply.github.com>
2025-07-18 20:07:37 -0500

    Refactor main.ts to integrate Pinia with persisted state and move AuraCustomPreset to a new file for better organization

--- src/main.ts
+++ src/main.ts
@@ -1,95 +1,23 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
+
 import App from './App.vue';
 import router from './router';
+
 import './styles/main.css';
 
-import { definePreset } from '@primeuix/themes';
-import Aura from '@primeuix/themes/aura';
+// Primevue
 import PrimeVue from 'primevue/config';
+import { AuraCustomPreset } from './styles/primevue.config';
 
-const AuraCustomPreset = definePreset(Aura, {
-  semantic: {
-    primary: {
-      50: '{amber.50}',
-      100: '{amber.100}',
-      200: '{amber.200}',
-      300: '{amber.300}',
-      400: '{amber.400}',
-      500: '{amber.500}',
-      600: '{amber.600}',
-      700: '{amber.700}',
-      800: '{amber.800}',
-      900: '{amber.900}',
-      950: '{amber.950}'
-    },
-    formField: {
-      paddingY: '0.4rem'
-    },
-    colorScheme: {
-      light: {
-        surface: {
-          0: '#ffffff',
-          50: '{zinc.50}',
-          100: '{zinc.100}',
-          200: '{zinc.200}',
-          300: '{zinc.300}',
-          400: '{zinc.400}',
-          500: '{zinc.500}',
-          600: '{zinc.600}',
-          700: '{zinc.700}',
-          800: '{zinc.800}',
-          900: '{zinc.900}',
-          950: '{zinc.950}'
-        }
-      },
-      dark: {
-        surface: {
-          0: '#ffffff',
-          50: '{neutral.50}',
-          100: '{neutral.100}',
-          200: '{neutral.200}',
-          300: '{neutral.300}',
-          400: '{neutral.400}',
-          500: '{neutral.500}',
-          600: '{neutral.600}',
-          700: '{neutral.700}',
-          800: '{neutral.800}',
-          900: '{neutral.900}',
-          950: '{neutral.950}'
-        }
-      }
-    }
-  },
-  components: {
-    inputtext: {
-      colorScheme: {
-        dark: {
-          root: {
-            background: 'transparent'
-          }
-        }
-      }
-    },
-    select: {
-      colorScheme: {
-        dark: {
-          root: {
-            background: 'transparent'
-          }
-        }
-      }
-    },
-    button: {
-      root: {
-        paddingY: '0.35rem'
-      }
-    }
-  }
-});
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
 
 const app = createApp(App);
 
 app.use(router);
+app.use(pinia);
 
 app.use(PrimeVue, {
   ripple: true,
